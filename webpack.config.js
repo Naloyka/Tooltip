dee8104c47c6577ea35233425a77d6e6826495e0
Naloyka <elena.naloeva@yandex.ru>
2022-10-04 00:05:33 +0300

    path-5

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '../',
+    publicPath: './src',
   },
   module: {
     rules: [
